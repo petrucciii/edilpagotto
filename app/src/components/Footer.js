377cb1df9eb53e970e48ba39071bf01d274d8f50
petrucciii <riccardo.petrucci99@gmail.com>
2021-05-01 12:37:16 +0200

    responsive-who-img

--- app/src/components/Footer.js
+++ app/src/components/Footer.js
@@ -4,6 +4,8 @@ import '../App.css';
 function Footer() {
   return (
     <div>
+      <br/>
+      <hr/>
       <footer className="footer">
         <div className="copyright">&copy; Copyright 2021. Tutti i diritti riservati.</div>
         <div id="credits">Powered by <a href="https://github.com/petrucciii"> Riccardo Petrucci</a></div>
