351bb10cc0f944cd3e26bd9df8aeb38e8d953346
petrucciii <riccardo.petrucci99@gmail.com>
2021-07-16 00:55:14 +0200

    map-better

--- app/src/components/home/Contact.js
+++ app/src/components/home/Contact.js
@@ -14,7 +14,7 @@ export default class Contact extends Component {
                 <iframe 
                     title="map"
                     id="map" 
-                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2796.7942888713796!2d12.01773531553216!3d45.494087079101256!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x477ece7e662595af%3A0x76e343a35f893511!2sVia%20Zinalbo%2C%2059%2C%2030036%20Santa%20Maria%20di%20sala%20VE!5e0!3m2!1sit!2sit!4v1626205134042!5m2!1sit!2sit" 
+                    src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d5520.755400891162!2d12.019924!3d45.494087!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x477ece7e662595af%3A0x76e343a35f893511!2sVia%20Zinalbo%2C%2059%2C%2030036%20Santa%20Maria%20di%20sala%20VE!5e1!3m2!1sit!2sit!4v1626389398526!5m2!1sit!2sit" 
                     width="1400" 
                     height="450" 
                     allowFullScreen="" 
